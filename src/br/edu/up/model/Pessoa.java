a4dd37befb2cc1bc5cd5e1dc4b0274759a63134a
Guilherme Silva <guizaomtz@gmail.com>
2024-06-24 20:00:22 -0300

    criação da classe pessoa

--- src/br/edu/up/model/Pessoa.java
+++ src/br/edu/up/model/Pessoa.java
@@ -3,8 +3,24 @@ package br.edu.up.model;
 public class Pessoa {
     private int codigo;
     private String nome;
-    private String rua;
-    private String cidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String toCsv(){
+        return codigo + ";" + nome + ";"; 
+    }
+    
 
     
 }
